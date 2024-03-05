025d35fcb3fea89f4e36cdd78c0f41c4f2199cde
Fenriz1349 <129265577+Fenriz1349@users.noreply.github.com>
2024-03-05 13:34:53 +0100

    Initial Commit

---
+++ MyPet/MyPetApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPetApp.swift
+//  MyPet
+//
+//  Created by apprenant68 on 05/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyPetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
